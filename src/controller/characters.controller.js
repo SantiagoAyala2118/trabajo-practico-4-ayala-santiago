5f772c1665dcb6ec38c32299cf754d6c7cfef94b
Santiago Ayala <santitomayala07@gmail.com>
2025-06-24 23:03:55 -0300

    Se avanzó en las validaciones y se agregaron unas pocas en el modelo

--- src/controller/characters.controller.js
+++ src/controller/characters.controller.js
@@ -3,90 +3,109 @@ import charactersRoutes from "../routes/characters.routes.js";
 
 
 //Validaciones
-export const createCharacter = async (req,res)=> {
+export const createCharacter = async (req, res) => {
     try {
         const { name, ki, raze, gender } = req.body;
 
-        if(!name || !ki || !raze || !gender){
-            res.json({
+        if (!name || !ki || !raze || !gender) {
+            res.status(400).json({
                 status: 400,
                 message: 'Todos los campos deben ser rellenados'
             });
         };
 
-        if(name==='' || ki==='' || raze==='' || gender===''){
-            res.json({
+        if (name === '' || ki === '' || raze === '' || gender === '') {
+            res.status(400).json({
                 status: 400,
                 message: 'Todos los campos deben ser rellenados'
             })
         }
 
-        if(ki === NaN){
+        if (ki === NaN) {
             console.log('El nivel de poder (ki) debe ser un numero entero')
-            res.json({
+            res.status(400).json({
                 status: 400,
                 message: 'El nivel de poder (ki) debe ser un numero entero'
             });
         };
 
-        if(gender != 'Male' && gender != 'Female'){
+        if (gender != 'Male' && gender != 'Female') {
             console.log('Solo se aceptan valores como "Male" o "Female"');
-            res.json({
+            res.status(400).json({
                 status: 400,
                 message: 'Solo se aceptan valores como "Male" o "Female"'
             });
         };
 
+        if (description !== undefined && typeof description !== 'string') {
+            res.status(400).json({
+                status: 400,
+                message: 'Si incluyes descripción, debe ser una cadena de texto.' 
+            });
+        };
 
 
+
+        //Pasó todas las validaciones
         const character = await Character.create({
             name,
             ki,
             raze,
-            gender  
+            gender
         });
-        res.status(201).json({character});
-    }catch (err){
+        res.status(201).json({ character });
+    } catch (err) {
         console.error('Se ha producido un error en la creación del personaje', err);
     };
 };
 
-export const getAllCharacters = async (req,res) => {
-    try{
+export const getAllCharacters = async (req, res) => {
+    try {
         const character = await Character.findAll();
-        res.status(200).json({character});
+        res.status(200).json({ character });
 
-    }catch (err){
+    } catch (err) {
         console.error('Se ha producido un error al traer todos los personajes', err);
     };
 };
 
 export const getOneCharacter = async (req, res) => {
-    try{
+    try {
         const character = await Character.findByPk(req.params.id);
-        if(character){
-            res.status(200).json({character});
-        }else{
+        if (character) {
+            res.status(200).json({ character });
+        } else {
             res.status(400).json({
                 message: 'El personaje no existe dentro de la base de datos',
             });
         };
-    }catch (err){
+    } catch (err) {
         console.error('Se produjo un error al traer el personaje seleccionado', err);
     };
 };
 
-export const updateCharacter = async (req,res) => {
-    try{
-        const characterUpdated = await Character.update(req.params.id);
-        if(characterUpdated){
-            res.status(200).json({characterUpdated})
-        }else{
+export const updateCharacter = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const character = await Character.findByPk(id);
+        if (character) {
+            await Character.update({
+                name,
+                ki,
+                race,
+                gender,
+                where: {
+                    id
+                }
+
+            });
+            res.status(204).json({ characterUpdated, exclude: [res.body.name] })
+        } else {
             res.status(404).json({
                 message: 'El personaje seleccionado no fue encontrado en la base de datos'
             });
         };
-    }catch (err){
+    } catch (err) {
         console.error('Hubo un error al actualizar el personaje seleccionado', err);
     };
 };
--- src/model/character.model.js
+++ src/model/character.model.js
@@ -2,10 +2,10 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
 const Character = sequelize.define('Character', {
-    name :{ type: DataTypes.STRING(200), allowNull: false },
+    name :{ type: DataTypes.STRING(200), allowNull: false, unique: true },
     ki: { type: DataTypes.INTEGER(), allowNull: false},
     raze: { type: DataTypes.STRING(150), allowNull: false, defaultValue: 'Human' },
-    gender: { type: DataTypes.STRING(100), allowNull: false },
+    gender: { type: DataTypes.ENUM('Male','Female'), allowNull: false },
     description: { type: DataTypes.STRING(1000) }
 });
 await Character.sync();
